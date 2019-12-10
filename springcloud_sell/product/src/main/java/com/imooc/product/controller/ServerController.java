c5ef157f7b5d1ff82fb891cbf2b093f2d2d8971d
menglingchong <menglingchong@aixin-ins.com>
2019-12-10 18:59:01 +0800

    产品服务中提供接口供订单服务调用

--- springcloud_sell/product/src/main/java/com/imooc/product/controller/ServerController.java
+++ springcloud_sell/product/src/main/java/com/imooc/product/controller/ServerController.java
@@ -1,8 +1,16 @@
 package com.imooc.product.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author ：menglc
  * @date ：Created in 2019/12/10 10:51
  */
+@RestController
 public class ServerController {
+    @GetMapping("/msg")
+    public String  msg() {
+        return "this is product' msg";
+    }
 }
