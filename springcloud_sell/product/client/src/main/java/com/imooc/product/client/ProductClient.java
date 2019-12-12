822351a10e47d2830e44e8b0b0f5b7da72b761f9
menglingchong <menglingchong@aixin-ins.com>
2019-12-12 20:19:38 +0800

    商品服务中修改client模块中feign的依赖

--- springcloud_sell/product/client/src/main/java/com/imooc/product/client/ProductClient.java
+++ springcloud_sell/product/client/src/main/java/com/imooc/product/client/ProductClient.java
@@ -2,10 +2,10 @@ package com.imooc.product.client;
 
 import com.imooc.product.common.DecreaseStockInput;
 import com.imooc.product.common.ProductInfoOutput;
-import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.cloud.openfeign.FeignClient;
 
 import java.util.List;
 
@@ -21,8 +21,7 @@ public interface ProductClient {
 
     @PostMapping("/product/listForOrder")
     public List<ProductInfoOutput> listForOrder(@RequestBody List<String> productIdList);
-    
+
     @PostMapping("/product/decreaseStock")
     public void decreaseStock(@RequestBody List<DecreaseStockInput> cartDTOList);
 }
- 
