ced928c63a00aae2a59592c5c1230253664cab67
menglingchong <menglingchong@aixin-ins.com>
2019-12-16 18:24:54 +0800

    添加refreshscope注解

--- springcloud_sell/order/server/src/main/java/com/imooc/order/server/controller/EventController.java
+++ springcloud_sell/order/server/src/main/java/com/imooc/order/server/controller/EventController.java
@@ -1,6 +1,7 @@
 package com.imooc.order.server.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/gitEnvironment")
+@RefreshScope
 public class EventController {
     @Value("${spring.profiles}")
     private String env;
