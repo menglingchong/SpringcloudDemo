b045973d4437296c98cda1dbd0d90c57b6005565
menglingchong <meng.lingchong@iwhalecloud.com>
2019-12-31 20:10:45 +0800

    订单服务提交

--- springcloud_sell/order/server/src/main/java/com/imooc/order/server/controller/OrderController.java
+++ springcloud_sell/order/server/src/main/java/com/imooc/order/server/controller/OrderController.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.CollectionUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
