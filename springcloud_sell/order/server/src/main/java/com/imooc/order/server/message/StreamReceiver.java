51a0739831b4f9c2e4d774407b5459270fc6187d
menglingchong <meng.lingchong@iwhalecloud.com>
2019-12-31 20:11:54 +0800

    订单服务提交

--- springcloud_sell/order/server/src/main/java/com/imooc/order/server/message/StreamReceiver.java
+++ springcloud_sell/order/server/src/main/java/com/imooc/order/server/message/StreamReceiver.java
@@ -1,8 +1,10 @@
 package com.imooc.order.server.message;
 
+import com.imooc.order.server.dto.OrderDTO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Component;
 
 /**
@@ -13,8 +15,21 @@ import org.springframework.stereotype.Component;
 @EnableBinding(StreamClient.class)
 @Slf4j
 public class StreamReceiver {
+    // @StreamListener(value = StreamClient.OUTPUT)
+    // public void process(Object message) {
+    //     log.info("StreamReceiver: {}",message);
+    // }
+
+    //接收orderDTO对象 消息
     @StreamListener(value = StreamClient.OUTPUT)
-    public void process(Object message) {
+    @SendTo(StreamClient.INPUT2)
+    public String process(OrderDTO message) {
         log.info("StreamReceiver: {}",message);
+        return "Received Message And Send To Input2";
+    }
+
+    @StreamListener(value = StreamClient.INPUT2)
+    public void process2(String message) {
+        log.info("StreamReceiver2: {}",message);
     }
 }
