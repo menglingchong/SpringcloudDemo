3550981d1548dde8062b38634f7213d860cecdaf
menglingchong <meng.lingchong@iwhalecloud.com>
2020-01-06 23:58:07 +0800

    订单服务添加订单完成接口

--- springcloud_sell/order/server/src/main/java/com/imooc/order/server/service/impl/OrderServiceImpl.java
+++ springcloud_sell/order/server/src/main/java/com/imooc/order/server/service/impl/OrderServiceImpl.java
@@ -5,6 +5,8 @@ import com.imooc.order.server.dataobject.OrderMaster;
 import com.imooc.order.server.dto.OrderDTO;
 import com.imooc.order.server.enums.OrderStatusEnum;
 import com.imooc.order.server.enums.PayStatusEnum;
+import com.imooc.order.server.enums.ResultEnum;
+import com.imooc.order.server.exception.OrderException;
 import com.imooc.order.server.repository.OrderDetailRepository;
 import com.imooc.order.server.repository.OrderMasterRepository;
 import com.imooc.order.server.service.OrderService;
@@ -15,11 +17,14 @@ import com.imooc.product.common.ProductInfoOutput;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
+import javax.transaction.Transactional;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 /**
@@ -34,7 +39,9 @@ public class OrderServiceImpl implements OrderService {
     private OrderDetailRepository orderDetailRepository;
     @Autowired
     private ProductClient productClient;
+
     @Override
+    @Transactional
     public OrderDTO create(OrderDTO orderDTO) {
         String orderId = KeyUtil.genUniqueKey();
         //查询商品信息(调用商品服务)
@@ -79,6 +86,35 @@ public class OrderServiceImpl implements OrderService {
         orderMaster.setCreateTime(new Date());
         orderMaster.setUpdateTime(new Date());
         orderMasterRepository.save(orderMaster);
+        return orderDTO;
+    }
+
+    @Override
+    @Transactional
+    public OrderDTO finish(String orderId) {
+        //1. 先查询订单
+        Optional<OrderMaster> orderMasterOptional = orderMasterRepository.findById(orderId);
+        if (!orderMasterOptional.isPresent()) {
+            throw new OrderException(ResultEnum.ORDER_NOT_EXIST);
+        }
+        //2. 判断订单的状态
+        OrderMaster orderMaster = orderMasterOptional.get();
+        Integer orderStatus = orderMaster.getOrderStatus();
+        if (!OrderStatusEnum.NEW.getCode().equals(orderStatus)) {
+            throw new OrderException(ResultEnum.ORDER_STATUS_ERROR);
+        }
+        //3. 修改订单状态为完结
+        orderMaster.setOrderStatus(OrderStatusEnum.FINISHED.getCode());
+        orderMasterRepository.save(orderMaster);
+        //4. 查询订单详情
+        List<OrderDetail> orderDetailList = orderDetailRepository.findByOrderId(orderId);
+        if (CollectionUtils.isEmpty(orderDetailList)) {
+            throw new OrderException(ResultEnum.ORDER_DETAIL_NOT_EXIST);
+        }
+
+        OrderDTO orderDTO = new OrderDTO();
+        BeanUtils.copyProperties(orderMaster,orderDTO);
+        orderDTO.setOrderDetailList(orderDetailList);
         return orderDTO;
     }
 }
